0fbed7941f72b99b9f55221867ff38712f40ba62
Ksusha132 <orlovaksusha@mail.ru>
2018-12-02 23:13:03 +0300

    booking service beginning

--- src/main/java/epam/cinemaProject/services/impl/BookingServiceImpl.java
+++ src/main/java/epam/cinemaProject/services/impl/BookingServiceImpl.java
@@ -1,10 +1,20 @@
 package epam.cinemaProject.services.impl;
 
 import epam.cinemaProject.pojo.cinema.Event;
+import epam.cinemaProject.services.DiscountService;
+import org.springframework.beans.factory.annotation.Required;
 
 import java.time.LocalDateTime;
 
 public class BookingServiceImpl implements epam.cinemaProject.services.BookingService{
+
+    private DiscountService discountService;
+
+    @Required
+    public void setDiscountService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
     @Override
     public Double getTicketPrice() {
         return null;
@@ -18,5 +28,9 @@ public class BookingServiceImpl implements epam.cinemaProject.services.BookingSe
     @Override
     public void getPurchasedTicketsForEvent(Event event, LocalDateTime dateTime) {
 
+    }
+
+    private boolean isAvailable() {
+        return false;
     }
 }
